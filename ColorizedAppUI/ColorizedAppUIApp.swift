cf7cbeeb07aaed75e9fa10f3fb9e746df6808c8c
Pavel <lmr_16@mail.ru>
2022-12-12 16:14:29 +0300

    Initial Commit

---
+++ ColorizedAppUI/ColorizedAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppUIApp.swift
+//  ColorizedAppUI
+//
+//  Created by user on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
